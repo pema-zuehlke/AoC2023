2ff1b1e27d099d5e33b7e35d75541f5a647d8618
pema-zuehlke <pedro.machado@zuehlke.com>
2024-02-06 13:34:41 +0000

    Update main function

--- AoC.cpp
+++ AoC.cpp
@@ -2,38 +2,33 @@
 
 #include "common/common.hpp"
 #include "exercises/day.hpp"
-#include "exercises/day1.hpp"
-
 
 using namespace std;
 constexpr int PARAMETERS_NEEDED = 1;
 
-extern "C"
-{
-    int main(int argc, char *argv[])
-    {   
-        std::string nameOfDay = std::string(argv[1]);
-        
-        if(argc != PARAMETERS_NEEDED + 1)
-        {
-            //do not have the correct parameters
-            log("Wrong number of parameters. expected:" << PARAMETERS_NEEDED << " received:" << (argc-1));
-            return -1; 
-        }
-
-        Day* exercise = ManageInput::daySearch(nameOfDay);
-
-        if(exercise == nullptr)
-        {
-            //day is not implemented
-            log("Day passed as parameter (" << nameOfDay << ") still not implemented");
-            return -2;
-        }
-        exercise->init();
-        
-        log("Return value is:" << exercise->execute());
-        delete exercise;
-        return 0;
+int main(int argc, char *argv[])
+{   
+    std::string nameOfDay = std::string(argv[1]);
+    
+    if(argc != PARAMETERS_NEEDED + 1)
+    {
+        //do not have the correct parameters
+        log("Wrong number of parameters. expected:" << PARAMETERS_NEEDED << " received:" << (argc-1));
+        return -1; 
     }
 
+    Day* exercise = ManageInput::daySearch(nameOfDay);
+
+    if(exercise == nullptr)
+    {
+        //day is not implemented
+        log("Day passed as parameter (" << nameOfDay << ") still not implemented");
+        return -2;
+    }
+    exercise->init();
+    
+    log("Return value is:" << exercise->execute());
+    delete exercise;
+    return 0;
 }
+
