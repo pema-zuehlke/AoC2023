21f732869c85b5e3aa5508a0d5e761b03d88348f
pema-zuehlke <pedro.machado@zuehlke.com>
2024-02-16 11:42:53 +0000

    Create the basic CMake project

---
+++ build/default/CMakeFiles/AoC2023.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pema/AoC2023/AoC.cpp" "CMakeFiles/AoC2023.dir/AoC.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/AoC.cpp.o.d"
+  "/home/pema/AoC2023/common/common.cpp" "CMakeFiles/AoC2023.dir/common/common.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/common/common.cpp.o.d"
+  "/home/pema/AoC2023/exercises/day1a.cpp" "CMakeFiles/AoC2023.dir/exercises/day1a.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/exercises/day1a.cpp.o.d"
+  "/home/pema/AoC2023/exercises/day1b.cpp" "CMakeFiles/AoC2023.dir/exercises/day1b.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/exercises/day1b.cpp.o.d"
+  "/home/pema/AoC2023/exercises/day2a.cpp" "CMakeFiles/AoC2023.dir/exercises/day2a.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/exercises/day2a.cpp.o.d"
+  "/home/pema/AoC2023/exercises/day2b.cpp" "CMakeFiles/AoC2023.dir/exercises/day2b.cpp.o" "gcc" "CMakeFiles/AoC2023.dir/exercises/day2b.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/default/CMakeFiles/AoC2023.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "AoC2023"
+  "AoC2023.pdb"
+  "CMakeFiles/AoC2023.dir/AoC.cpp.o"
+  "CMakeFiles/AoC2023.dir/AoC.cpp.o.d"
+  "CMakeFiles/AoC2023.dir/common/common.cpp.o"
+  "CMakeFiles/AoC2023.dir/common/common.cpp.o.d"
+  "CMakeFiles/AoC2023.dir/exercises/day1a.cpp.o"
+  "CMakeFiles/AoC2023.dir/exercises/day1a.cpp.o.d"
+  "CMakeFiles/AoC2023.dir/exercises/day1b.cpp.o"
+  "CMakeFiles/AoC2023.dir/exercises/day1b.cpp.o.d"
+  "CMakeFiles/AoC2023.dir/exercises/day2a.cpp.o"
+  "CMakeFiles/AoC2023.dir/exercises/day2a.cpp.o.d"
+  "CMakeFiles/AoC2023.dir/exercises/day2b.cpp.o"
+  "CMakeFiles/AoC2023.dir/exercises/day2b.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AoC2023.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
